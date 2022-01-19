2848df3615b8ad4d923e17d29ae1475cf89433c2
JuliaaMuller <julialm.muller@gmail.com>
2022-01-18 21:00:05 -0500

    play.js refactored

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here,
+    port: 50541// PORT number here,
+  });
+  conn.on("connect", () => {
+    console.log("Hi you are connected")
+  });
+  conn.on('data', (data) => { // interpret incoming data as text
+    console.log(data.toString());
+    // conn.end()
+  });
+  conn.on("end", () => {
+    console.log("Disconnected from server")
+  });
+};
+  module.exports = connect;
--- play.js
+++ play.js
@@ -1,26 +1,4 @@
 const net = require("net");
+const connect = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541// PORT number here,
-  });
-  conn.on("connect", () => {
-    console.log("Hi you are connected")
-  });
-  conn.on('data', (data) => { // interpret incoming data as text
-    console.log(data.toString());
-    // conn.end()
-  });
-  conn.on("end", () => {
-    console.log("Disconnected from server")
-  });
-  
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+  connect();
